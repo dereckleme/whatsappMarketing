9b2d99024394989f12b19cf80e8c5ca791f08067
Administrador <Administrador@Dereck>
2014-06-23 01:09:26 -0300

    Finalização modelagem banco de dados, atualização conexão com banco de dados.

--- config/autoload/doctrine.local.php
+++ config/autoload/doctrine.local.php
@@ -9,7 +9,7 @@ return array(
 										'port'     => '3306',
 										'user'     => 'root',
 										'password' => '',
-										'dbname'   => 'BaseDeDados',
+										'dbname'   => 'whatsapp',
 								    'charset' => 'utf8',
 								    'driverOptions' => array(
 								    		1002=>'SET NAMES utf8'
